a5647e497ef67601aab935b04dbb95d3d9dde88c
russel-zaman <kazi_366953@gmx.de>
2024-03-07 19:05:42 +0100

    update expression

--- P4_prime_factorial/prime_factors.py
+++ P4_prime_factorial/prime_factors.py
@@ -1,11 +1,29 @@
 # Lambda function to find factors of a number
 factors = lambda n: [x for x in range(1, n+1) if not n % x]
 
+# regular expression
+# def factors(n):
+#     factor_list = []
+#     for x in range(1, n+1):
+#         if not n % x:
+#             factor_list.append(x)
+#     return factor_list
+
+
 # Lambda function to check if a number is prime
 is_prime = lambda n: len(factors(n)) == 2
 
+# regular expression
+# def is_prime_regular(n):
+#     return len(factors(n)) == 2
+
 # Lambda function to filter prime factors from the list of factors
 primefactors = lambda n: list(filter(is_prime, factors(n)))
+
+# regular expression
+# def primefactors_regular(n):
+#     return list(filter(is_prime_regular, factors(n)))
+
 
 # Function to perform prime factorization recursively
 def primeFactorize(n):
